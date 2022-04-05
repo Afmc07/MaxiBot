a4d4685a65720c1d386fae876ecb0c18e4c028f7
Afmc07 <afmc07@hotmail.com>
2022-04-04 21:32:14 -0300

    Implement Basico Commands

--- lib/maxibot/consumer.ex
+++ lib/maxibot/consumer.ex
@@ -7,6 +7,29 @@ defmodule Maxi.Consumer do
     Consumer.start_link(__MODULE__)
   end
 
+  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+    if msg.author.username == "lunaジュ" do
+      handle_mom(msg)
+    else
+      handle_others(msg)
+    end
+  end
+
+  def handle_mom(msg) do
+    case msg.content do
+      ">hello" -> Api.create_message(msg.channel_id, "HI MOM! I LOVE YOU!!")
+      _ -> :ignore
+    end
+  end
+
+  def handle_others(msg) do
+    case msg.content do
+      ">hello" -> Api.create_message(msg.channel_id, "HI!")
+      ">tst" -> IO.puts(msg)
+      _ -> :ignore
+    end
+  end
+
   def handle_event(_event) do
     :noop
   end
